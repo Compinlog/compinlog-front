4ffd1c215d637b16b277af96ed961596af73359c
JEsteban2D <juanestebancossio21@gmail.com>
2023-03-22 17:14:42 -0500

    fic Home View

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script>
+    export default {
+    name: "Home-view", // Nombre del componente
+    data() {
+    // definición de variables y banderas
+    return {};
+    },
+    // estados o propiedades
+};
+</script>
+
+<template>
+    <div>
+    
+    </div>
+</template>
